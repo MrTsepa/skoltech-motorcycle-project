ae04229054e2a4071052f6357256804ed57b7f66
Luke <work37@gmail.com>
2018-09-27 12:58:42 +0300

    websocket + json

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import os
 
 import websockets
@@ -8,28 +9,47 @@ from pymodbus.client.sync import ModbusSerialClient
 from x3emotor import X3EMotor
 
 
-async def setAngleSlowly(max_speed=20):
+async def setAngle(max_speed=20):
     global desiredAngle
     while True:
+        print("New desired angle:", desiredAngle)
         prevDesiredAngle = desiredAngle
-        flag = False
+        finished = False
         while desiredAngle == prevDesiredAngle:
             currentAngle = M.readAngle()
             dist = abs(currentAngle - desiredAngle)
-            if flag:
+            if finished:
                 pass
             elif dist < 500:
                 M.setAngle(desiredAngle)
-                flag = True
-            # else:
-            #     K = 0.05
-            #     print("V:", min(max_speed, dist*K))
-            #     M.setSpeed(-min(max_speed, dist*K) if desiredAngle < currentAngle else min(max_speed, dist*K))
+                finished = True
             elif dist < max_speed * 30:
                 M.setSpeed(-5 if desiredAngle < currentAngle else 5)
             else:
                 M.setSpeed(-max_speed if desiredAngle < currentAngle else max_speed)
             await asyncio.sleep(0.01)
+
+async def setP():
+    global desiredP, M
+    M.writeRegister(16, desiredP)
+    currentP = desiredP
+    while True:
+        if desiredP != currentP:
+            print("New desired P:", desiredP)
+            M.writeRegister(16, desiredP)
+            currentP = desiredP
+        await asyncio.sleep(0.1)
+
+
+async def setI():
+    global desiredI, M
+    M.writeRegister(15, desiredI)
+    currentI = desiredI
+    while True:
+        if desiredI != currentI:
+            M.writeRegister(16, desiredI)
+            currentI = desiredI
+        await asyncio.sleep(0.1)
 
 
 async def websocketSender(websocket):
@@ -37,17 +57,18 @@ async def websocketSender(websocket):
     while True:
         angle = M.readAngle()
         print(angle)
-        await asyncio.wait_for(websocket.send(str(angle)), 1)
+        await asyncio.wait_for(websocket.send(json.dumps({'angle': angle})), 1)
         await asyncio.sleep(0.1)
 
 
 async def websocketReceiver(websocket):
-    global desiredAngle
+    global desiredAngle, desiredP
     async for message in websocket:
         try:
-            desiredAngle = int(message)
-            print("New desired angle:", desiredAngle)
-        except ValueError as e:
+            data = json.loads(message)
+            desiredAngle = int(data['angle'])
+            desiredP = int(data['P'])
+        except Exception as e:
             print(e)
 
 
@@ -65,29 +86,30 @@ async def websocketHandler(websocket, path):
         task.cancel()
 
 
-def setup():
-    global M
-    M.writeRegister(16, 500)  # P
-    M.writeRegister(15, 0)  # I
-
-
 MODBUS_PORT = os.getenv('MODBUS_PORT', default='COM14')
 # one speed unit is ~173 angle units per sec
 # 360 degrees is ~4000 angle units
 
 desiredAngle = 0
+desiredP = 100
+desiredI = 0
 
 client = ModbusSerialClient(method="rtu", port=MODBUS_PORT, stopbits=1,
                             bytesize=8, parity='N', baudrate=115200,
                             timeout=0.1)
 M = X3EMotor(client, 1)
 
-setup()
+M.writeRegister(16, desiredP)
+M.writeRegister(15, desiredI)
+
+startingAngle = M.readAngle()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(websockets.serve(websocketHandler, 'localhost', 8765))
 asyncio.gather(
-    setAngleSlowly()
+    websockets.serve(websocketHandler, 'localhost', 8765),
+    setAngle(),
+    setP(),
+    setI()
 )
 try:
     pass
